0281503e4ec665b6ac78714af3ea7c0d1111731d
a-friendly-ghost <mishanandgov@gmail.com>
2023-06-12 20:16:26 +0300

    improved codestyle thanks to nano the only person who wouldn't just say my code was fine because it worked and actually told me honestly how bad it is and how to fix it god bless

--- main.py
+++ main.py
@@ -26,6 +26,15 @@ if sys.platform == 'win32':
 
 ready = False
 
+async def get_webhook_for_channel(channel: discord.TextChannel):
+	# webhooks that we own have a non-None token attribute
+	for webhook in await channel.webhooks():
+		if webhook.token:
+			return webhook
+
+	print(f"Making webhook for {channel.name}...")
+	return await channel.create_webhook(name="mishnet webhook", reason="what are you a cop?")
+
 @client.event
 async def on_ready():
 	print('on ready begin')
@@ -124,104 +133,6 @@ async def on_ready():
 	ready = True
 	print('on ready end')
 
-@client.event
-async def on_message(message: discord.Message):
-
-	while ready == False:
-		await asyncio.sleep(0.1)
-
-	mishnet_channel = None
-	for group in mishnet_channels: # feeling like this would be another application for the associations data structure, but we made it only work for storing messages
-		if message.channel in group:
-			mishnet_channel = group
-
-			if message.webhook_id: # avoids unnecessarily checking all this
-				channelWebhooks = await message.channel.webhooks()
-				for webhook in channelWebhooks:
-					if webhook.id == message.webhook_id:
-						if webhook.token:
-							# this webhook is from mishnet
-							return
-
-			if message.author.id in banlist:
-				try:
-					await message.delete()
-				except:
-					pass
-				return
-
-			if message.content == prefix + "perftest": 
-				startTime = time.perf_counter()
-			
-			target_channels = [i for i in mishnet_channel if i.guild != message.channel.guild]
-
-			name = await get_mishnick_or_username(conn, message.author) + ', from ' + serverNames[message.channel]
-
-			pfp = message.author.display_avatar.url
-			# run every message sending thingy in parallel
-			replied_message = await get_replied_message(message)
-			duplicate_messages = await asyncio.gather(*[bridge(message , channel , replied_message , name , pfp) for channel in target_channels])
-			try:
-				associations.set_duplicates(message, duplicate_messages)
-			except TheOriginalMessageHasAlreadyBeenDeletedYouSlowIdiotError:
-				# fuck shit piss delete them all again ig
-				await asyncio.gather(*[message.delete() for message in duplicate_messages])
-
-			if message.content == prefix + "perftest":
-				endTime = time.perf_counter()
-				bridge_time = "bridge time: " + str(endTime - startTime)
-				await message.channel.send(f"{bridge_time}s")
-
-	if message.content.startswith(prefix+'help') or message.content.startswith(prefix+'info'):
-		return await message.channel.send(f"""
-		__mishnet commands:__
-		{prefix}help - sends this message (alias: {prefix}info)
-		{prefix}perftest - tests bridge performance time
-		{prefix}nick [nick here] - changes your mishnet nickname (alias: {prefix}nickname)
-		{prefix}nick - tells you how other servers see your name (alias: {prefix}nicktest)
-		{prefix}clearnick - resets your mishnet nickname to use your username
-		the []s aren't part of the command
-		""")
-	
-	if message.content == prefix + "uwu": #vitally important command
-		await message.channel.send('uwu')
-
-	if message.content.startswith(prefix+"nick") or message.content.startswith(prefix+"nickname"): # both nick(change) and nick(test)
-		nick = message.content.replace(prefix+"nick ",'').replace(prefix+"nickname",'') # this is a bad (ugly) way to do this i think i should write a function
-
-		if message.content == prefix+"nick" or message.content.replace(prefix+"nick",'') == 'test': # nick(test) command
-			other_server_name = await get_mishnick_or_username(conn, message.author)
-			return await message.channel.send(f"hi!!! your name will be seen by people on other servers as {other_server_name} ! :D")
-
-		# nick(change) command
-
-		if len(nick) > 32:
-			return await message.channel.send('sorry, a mishnet nickname can only be a maximum of 32 characters long')
-		
-		async with conn.cursor(row_factory=psycopg.rows.dict_row) as cursor:
-			await cursor.execute('SELECT user_id, nickname FROM nicknames WHERE user_id = %s' , [message.author.id])
-			record = await cursor.fetchone()
-			if record:
-				await cursor.execute(f"UPDATE nicknames SET nickname=%s WHERE user_id=%s" , [nick,message.author.id])
-			else:
-				await cursor.execute(f"INSERT INTO nicknames (user_id, nickname) VALUES (%s, %s)" , [message.author.id,nick])
-
-		await conn.commit()
-		await message.channel.send(f'hello {nick}! i have set your mishnet (me) nickname as `{nick}` c:')
-
-	if message.content.startswith(prefix+"clearnick"):
-		async with conn.cursor(row_factory=psycopg.rows.dict_row) as cursor:
-			await cursor.execute('SELECT user_id, nickname FROM nicknames WHERE user_id = %s' , [message.author.id])
-			record = await cursor.fetchone()
-			if record:
-				await cursor.execute(f"DELETE FROM nicknames WHERE user_id=%s",[message.author.id])
-				await message.channel.send(f'your mishnet nickname has been cleared, {message.author.name}! it will now show up to others as your discord username ^-^')
-			else:
-				await message.channel.send(f'{message.author.name}, your mishnet nickname is already the default (your username) ! :p')
-		await conn.commit()
-
-	return
-
 async def get_replied_message(original_message: discord.Message) -> discord.Message:
 	replied_message_reference = original_message.reference
 	if replied_message_reference:
@@ -271,36 +182,119 @@ async def bridge(original_message: discord.Message, target_channel: discord.Text
 
 	to_send = await create_to_send(original_message, target_channel, replied_message)
 
-	if original_message.attachments:
-		attachments_to_files = await asyncio.gather(*[attachment.to_file() for attachment in original_message.attachments])
-		copy_message = await webhook.send(
-			allowed_mentions=discord.AllowedMentions.none(), 
-			content=to_send, 
-			username=name, 
-			avatar_url=pfp, 
-			wait=True,
-			files=attachments_to_files
-		)
-	else:
-		copy_message = await webhook.send(
-			allowed_mentions=discord.AllowedMentions.none(), 
-			content=to_send, 
-			username=name, 
-			avatar_url=pfp, 
-			wait=True,
-		)
+	attachments_to_files = await asyncio.gather(*[attachment.to_file() for attachment in original_message.attachments])
 	
-	assert copy_message is not None
+	copy_message = await webhook.send(
+		allowed_mentions=discord.AllowedMentions.none(), 
+		content=to_send, 
+		username=name, 
+		avatar_url=pfp, 
+		wait=True,
+		files=attachments_to_files if attachments_to_files != [] else discord.utils.MISSING # i hate this
+	)
+	
 	return copy_message
 
-async def get_webhook_for_channel(channel: discord.TextChannel):
-	# webhooks that we own have a non-None token attribute
-	for webhook in await channel.webhooks():
-		if webhook.token:
-			return webhook
+@client.event
+async def on_message(message: discord.Message):
 
-	print(f"Making webhook for {channel.name}...")
-	return await channel.create_webhook(name="mishnet webhook", reason="what are you a cop?")
+	while ready == False:
+		await asyncio.sleep(0.1)
+
+	# commands
+
+	if message.content.startswith(prefix+'help') or message.content.startswith(prefix+'info'):
+		await message.channel.send(f"""
+		__mishnet commands:__
+		{prefix}help - sends this message (alias: {prefix}info)
+		{prefix}perftest - tests bridge performance time
+		{prefix}nick [nick here] - changes your mishnet nickname (alias: {prefix}nickname)
+		{prefix}nick - tells you how other servers see your name (alias: {prefix}nicktest)
+		{prefix}clearnick - resets your mishnet nickname to use your username
+		the []s aren't part of the command
+		""")
+	
+	if message.content == prefix + "uwu": #vitally important command
+		await message.channel.send('uwu')
+
+	if message.content.startswith(prefix+"nick") or message.content.startswith(prefix+"nickname"): # both nick(change) and nick(test)
+		nick = message.content.replace(prefix+"nick ",'').replace(prefix+"nickname",'') # this is a bad (ugly) way to do this i think i should write a function
+
+		if message.content == prefix+"nick" or message.content.replace(prefix+"nick",'') == 'test': # nick(test) command
+			other_server_name = await get_mishnick_or_username(conn, message.author)
+			await message.channel.send(f"hi!!! your name will be seen by people on other servers as {other_server_name} ! :D")
+
+		# nick(change) command
+
+		if len(nick) > 32:
+			await message.channel.send('sorry, a mishnet nickname can only be a maximum of 32 characters long')
+		
+		async with conn.cursor(row_factory=psycopg.rows.dict_row) as cursor:
+			await cursor.execute('SELECT user_id, nickname FROM nicknames WHERE user_id = %s' , [message.author.id])
+			record = await cursor.fetchone()
+			if record:
+				await cursor.execute(f"UPDATE nicknames SET nickname=%s WHERE user_id=%s" , [nick,message.author.id])
+			else:
+				await cursor.execute(f"INSERT INTO nicknames (user_id, nickname) VALUES (%s, %s)" , [message.author.id,nick])
+
+		await conn.commit()
+		await message.channel.send(f'hello {nick}! i have set your mishnet (me) nickname as `{nick}` c:')
+
+	if message.content.startswith(prefix+"clearnick"):
+		async with conn.cursor(row_factory=psycopg.rows.dict_row) as cursor:
+			await cursor.execute('SELECT user_id, nickname FROM nicknames WHERE user_id = %s' , [message.author.id])
+			record = await cursor.fetchone()
+			if record:
+				await cursor.execute(f"DELETE FROM nicknames WHERE user_id=%s",[message.author.id])
+				await message.channel.send(f'your mishnet nickname has been cleared, {message.author.name}! it will now show up to others as your discord username ^-^')
+			else:
+				await message.channel.send(f'{message.author.name}, your mishnet nickname is already the default (your username) ! :p')
+		await conn.commit()
+
+	# bridge
+
+	mishnet_channel = next( (channel_group for channel_group in mishnet_channels if message.channel in channel_group) , None ) # feeling like this would be another application for the associations data structure, but we made it only work for storing messages
+	if not mishnet_channel:
+		return	
+
+	# ensure not bridging bridged messages themselves
+	if message.webhook_id: # avoids unnecessarily checking all this
+		channelWebhooks = await message.channel.webhooks()
+		for webhook in channelWebhooks:
+			if webhook.id == message.webhook_id:
+				if webhook.token:
+					# this webhook is from mishnet
+					return
+
+	if message.author.id in banlist:
+		try:
+			return await message.delete()
+		except:
+			return message.channel.send('error: mishnet does not have message deletion perms in this server, and so it cannot stop banned users')
+
+	if message.content == prefix + "perftest": 
+		startTime = time.perf_counter()
+	
+	target_channels = [i for i in mishnet_channel if i.guild != message.channel.guild]
+
+	name = await get_mishnick_or_username(conn, message.author) + ', from ' + serverNames[message.channel]
+
+	pfp = message.author.display_avatar.url
+	# run every message sending thingy in parallel
+	replied_message = await get_replied_message(message)
+	duplicate_messages = await asyncio.gather(*[bridge(message , channel , replied_message , name , pfp) for channel in target_channels])
+	try:
+		associations.set_duplicates(message, duplicate_messages)
+	except TheOriginalMessageHasAlreadyBeenDeletedYouSlowIdiotError:
+		# fuck shit piss delete them all again ig
+		await asyncio.gather(*[message.delete() for message in duplicate_messages])
+
+	if message.content == prefix + "perftest":
+		endTime = time.perf_counter()
+		bridge_time = "bridge time: " + str(endTime - startTime)
+		await message.channel.send(f"{bridge_time}s")
+
+	return
 
 @client.event
 async def on_message_delete(message: discord.Message):
